22180d7d1ddbb60f698573d07ff30325cb69bb56
barbarahw <127110674+barbarahw@users.noreply.github.com>
2024-10-29 09:27:08 -0300

    commit1

---
+++ src/controllers/login.controller.php
@@ -0,0 +1,3 @@
+<?php 
+
+include ('../models/login.model.php');
---
+++ src/views/login.view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Login</h1>
+
+    <form method="post" >
+
+        <label>Usuário: </label><input type="text" name="usuario">
+        <label>Senha: </label><input type="text" name="senha">
+
+        <button>Enviar</button>
+
+    </form>
+    <p>Não possui uma conta?</p>
+    <!-- link para cadastro -->
+</body>
+</html>

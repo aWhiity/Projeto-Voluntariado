bc0de78c781ed698082557ed1544e2a9b9ce62e7
Milena Alves <87149523+aWhiity@users.noreply.github.com>
2024-10-31 16:25:04 -0300

    model cadastrar voluntario

--- src/models/voluntario.model.php
+++ src/models/voluntario.model.php
@@ -3,17 +3,26 @@
 
     class voluntariado {
         private $pdo;
+        
 
         public function __construct($pdo) {
             $this->pdo = $pdo;
+
         }
         
-        public function cadastrar($nome, $email, $senha) {
-            $st = $this->pdo->prepare("insert into voluntario (nome, email, senha)");
-            return $st->execute([$nome, $email, $senha]);
+        public function cadastrar($objetoVoluntario) {
+            $query = $this->pdo->prepare("insert into voluntario (:nome, :cpf, :telefone, :email, :senha, :dataCriacao");
+            $query->bindParam(":nome", $objetoVoluntario->getNome());
+            $query->bindParam(":cpf", $objetoVoluntario->getCpf());
+            $query->bindParam(":telefone", $objetoVoluntario->getTelefone());
+            $query->bindParam("email", $objetoVoluntario->getEmail());
+            $query->bindParam("senha", $objetoVoluntario->getSenha());
+            $query->bindParam("dataCriacao", date('d/m/Y'));
+
+            $query->execute();
         }
 
+        
     }
-
 
 ?>
